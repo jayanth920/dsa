3f266bcfb9f09222257615a3a97821a276b0520a
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-06-29 00:41:35 -0500

    Armstrong number

--- Math/#1134 Armstrong Number/main.py
+++ Math/#1134 Armstrong Number/main.py
@@ -1,7 +1,23 @@
+# O(n)
+# def armstrong(n):
+#     new = str(n)
+#     sum = 0
+#     for i in range(len(new)):
+#         sum = sum + int(new[i])**3
+#     print(sum)
+# armstrong(153)
+
+
+ #O(logn + 1)
+
 def armstrong(n):
-    new = str(n)
+    num = n
+    l = len(str(n))
     sum = 0
-    for i in range(len(new)):
-        sum = sum + int(new[i])**3
-    print(sum)
-armstrong(153)
+    while(num>0):
+        ld = num % 10
+        sum = sum + ld**l
+        num = n//10
+    return sum
+print(armstrong(234))
+
