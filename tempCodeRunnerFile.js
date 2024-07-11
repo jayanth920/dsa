4d070438bffcd83dd2a968011ca1b24ba1301a92
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-07-10 22:00:15 -0500

    update temp

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-function gcdandlcm(n1,n2){
-//     let sm = Math.min(n1,n2)
-//     let bg = Math.max(n1,n2)
-//     gcdandlcm(sm,bg-sm)
-//     if(sm == 0) return sm
-
-// }
-
-// console.log(gcdandlcm(20, 15))
