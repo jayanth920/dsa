c368fc087230c47d0b70da523989295f8649880d
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-07-10 19:05:35 -0500

    #189 Rotate Array

--- Arrays/#189 Rotate Array/main.py
+++ Arrays/#189 Rotate Array/main.py
@@ -1,7 +1,17 @@
-# Solution 1 - O(n_
+# Solution 1 - O(n,k)
+# def rotate(nums,k):
+#     for i in range(k):
+#         nums.insert(0 ,nums.pop())
+#     return nums
+
+# print(rotate([1,2,3,4,5,6,7],3))
+
+# Solution - O(n)
 def rotate(nums,k):
-    for i in range(k):
-        nums.insert(0 ,nums.pop())
+    n = len(nums)
+    k = k%n
+    nums= nums[n-k:] + nums[:n-k]
     return nums
 
-print(rotate([1,2,3,4,5,6,7],3))
+print(rotate([1,2,3,4,5,6,7,8],4))
+# [5,6,7,8,1,2,3,4]
