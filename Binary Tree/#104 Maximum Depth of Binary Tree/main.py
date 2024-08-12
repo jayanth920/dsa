3714a5283b0c921de3ab8a74cded24980c2c68e3
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-08-12 14:00:24 -0500

    104

---
+++ Binary Tree/#104 Maximum Depth of Binary Tree/main.py
@@ -0,0 +1,6 @@
+def maxDepth(root):
+    if root is None:
+        return 0
+    left_depth = maxDepth(root.left)
+    right_depth = maxDepth(root.right)
+    return max(left_depth, right_depth) + 1
