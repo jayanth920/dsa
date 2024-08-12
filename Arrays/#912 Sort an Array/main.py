e74628a7b371deda3dcaf3e04eff0c2ddd36dcc3
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-08-12 15:46:17 -0500

    912

---
+++ Arrays/#912 Sort an Array/main.py
@@ -0,0 +1,33 @@
+def sortArray(nums):
+    def merge(arr):
+        if len(arr) > 1:
+            mid = len(arr) // 2
+
+            left = arr[:mid]
+            right = arr[mid:]
+
+            merge(left)
+            merge(right)
+
+            i = j = k = 0
+
+            while i < len(left) and j < len(right):
+                if left[i] < right[j]:
+                    arr[k] = left[i]
+                    i += 1
+                else:
+                    arr[k] = right[j]
+                    j += 1
+                k += 1
+            while i < len(left):
+                arr[k] = left[i]
+                i += 1
+                k += 1
+
+            while j < len(right):
+                arr[k] = right[j]
+                j += 1
+                k += 1
+
+    merge(nums)
+    return nums
